65f736b6cda0180624524ba1c592da4f303fa4d7
recko10 <adith.reddi@gmail.com>
2020-07-17 17:23:51 -0400

    added in coefficient analysis

--- trial_1/classifier_trial1.py
+++ trial_1/classifier_trial1.py
@@ -6,6 +6,9 @@ from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectFdr, chi2
+from sklearn.feature_selection import SelectKBest
 
 #Preprocess data
 featuresDf = pd.read_csv('taxonomic_abundances.csv') #Load in df
@@ -41,7 +44,42 @@ X = featuresDf.drop(columns = 'Experiment')
 #Convert the metadata column into a list of labels
 Y = finalFeaturesDf['Experiment'].tolist()
 
-#CONVERT ALL NUMBERS INTO 'yes' or 'no' values indicating the presence of the bacteria (yes is 1 and no is 0)
+#Identify important features--takes a list of coefficients, a list of all the bacteria, and a prescaled feature dataframe as input
+def featureImportanceRegression(model, bacteria, X_prescale):
+	importantBacteria = []
+	coefficientList = model.coef_.tolist()[0] 
+	#Identify bacteria with most impact on the model by identifying coefficients of high magnitude
+	for index in range(len(coefficientList)):
+		if coefficientList[index] < -0.40:
+			importantBacteria.append(bacteria[index])
+
+	print(f'Most impactful bacteria: {importantBacteria}\n')
+	print(f'Number of most impactful bacteria: {len(importantBacteria)}')
+	# X_feature_selected = SelectKBest(chi2, k=20).fit_transform(X, Y)
+	# importantFeatureData = X_feature_selected.tolist()
+
+	# importantBacteria = []
+	# for item in importantFeatureData:
+	# 	for column in X.columns.tolist():
+	# 		if item == X[column].tolist():
+	# 			importantBacteria.append(column)
+	# print(importantBacteria)
+
+
+#Scales features and then maps them onto a new vector space
+def pca(X):
+
+	indices = X.index #Save X inidces to import into principalDf once it is created
+	X = StandardScaler().fit_transform(X) #Scale the data
+
+	pca = PCA(n_components=2)
+	principalComponents = pca.fit_transform(X) #Transform the scaled data onto a new vector space
+	principalDf = pd.DataFrame(data=principalComponents, columns = ['principal component 1', 'principal component 2']) #Create new dataframe with principal components as the data
+
+	principalDf.index = indices
+	return principalDf
+
+#Convert all numbers into 'yes' or 'no' values indicating the presence of the bacteria (yes is 1 and no is 0)
 #This method is a little slow, but it works
 def binaryData(X, threshold):
 	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33) #Train-test split
@@ -70,6 +108,8 @@ def kneighbors(X, Y):
 
 def logisticRegeression(X, Y, loso=False, losoFeatureTrain = pd.DataFrame(['Empty']), losoTargetTrain = [], losoFeaturePredict = pd.DataFrame(['Empty']), losoTargetPredict = []):
 	if loso == False:
+		X_prescale = X
+		bacteria = X_prescale.columns.tolist()
 		X = StandardScaler().fit_transform(X) #Scale the data
 		X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33) #Train-test split
 
@@ -100,6 +140,9 @@ def logisticRegeression(X, Y, loso=False, losoFeatureTrain = pd.DataFrame(['Empt
 	print(f'Accuracy score: {accuracy_score(y_test,y_pred)}')
 	print(f'Confusion matrix: {confusion_matrix(y_test,y_pred)}')
 	print(f'AUROC score: {roc_auc_score(y_test, y_pred_roc)}\n')
+
+	#Identify important features
+	featureImportanceRegression(logReg, bacteria, X_prescale)
 
 def lassoRegression(X, Y):
 	X = StandardScaler().fit_transform(X) #Scale the data
@@ -206,7 +249,7 @@ def LOSONoUS(X,Y):
 	X['Experiment'] = Y
 
 	#Remove US dataset
-	X = X[~X.index.str.contains("MMRS")]
+	X = X[~X.index.str.contains('MMRS')]
 
 	#CCIS processing
 	CCISList = [item for item in X.index.tolist() if 'CCIS' in item] #Get all rows with CCIS in the index
@@ -258,15 +301,74 @@ def LOSONoUS(X,Y):
 	#Call return logistic regression function calls
 	return logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_1, losoTargetTrain=y_temp_1, losoFeaturePredict=CCIS, losoTargetPredict=CCIS_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_2, losoTargetTrain=y_temp_2, losoFeaturePredict=CCMD, losoTargetPredict=CCMD_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_3, losoTargetTrain=y_temp_3, losoFeaturePredict=ERR, losoTargetPredict=ERR_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_4, losoTargetTrain=y_temp_4, losoFeaturePredict=SAMEA, losoTargetPredict=SAMEA_targets)
 
+def LOSONoFrance(X,Y):
+
+	#Add targets back to features dataframe so that they can also be deleted when rows are left out
+	X['Experiment'] = Y
+
+	#Remove US dataset
+	X = X[~X.index.str.contains('CCIS')]
+
+	#CCMD processing
+	CCMDList = [item for item in X.index.tolist() if 'CCMD' in item] #Get all rows with CCMD in the index
+	X_temp_2 = X.drop(CCMDList, axis=0) #Feature training set
+	y_temp_2 = X_temp_2['Experiment'].tolist() #Target training set
+	X_temp_2 = X_temp_2.drop('Experiment',axis=1)
+
+	CCMD = X.iloc[0:114] 
+	CCMD_targets = CCMD['Experiment'] #Target prediction set
+	CCMD = CCMD.drop('Experiment', axis=1) #Feature prediction set
+
+	#ERR processing
+	ERRList = [item for item in X.index.tolist() if 'ERR' in item] #Get all rows with ERR in the index
+	X_temp_3 = X.drop(ERRList, axis=0) #Feature training set
+	y_temp_3 = X_temp_3['Experiment'].tolist() #Target training set
+	X_temp_3 = X_temp_3.drop('Experiment',axis=1)
+
+	ERR = X.iloc[114:234] 
+	ERR_targets = ERR['Experiment'] #Target prediction set
+	ERR = ERR.drop('Experiment', axis=1) #Feature prediction set
+
+	#MMRS processing
+	MMRSList = [item for item in X.index.tolist() if 'MMRS' in item] #Get all rows with MMRS in the index
+	X_temp_4 = X.drop(MMRSList, axis=0) #Feature training set
+	y_temp_4 = X_temp_4['Experiment'].tolist() #Target training set
+	X_temp_4 = X_temp_4.drop('Experiment',axis=1)
+
+	MMRS = X.iloc[234:362] 
+	MMRS_targets = MMRS['Experiment'] #Target prediction set
+	MMRS = MMRS.drop('Experiment', axis=1) #Feature prediction set
+
+
+	#SAMEA processing
+	SAMEAList = [item for item in X.index.tolist() if 'SAMEA' in item] #Get all rows with SAMEA in the index
+	X_temp_5 = X.drop(SAMEAList, axis=0) #Feature training set
+	y_temp_5 = X_temp_5['Experiment'].tolist() #Target training set
+	X_temp_5 = X_temp_5.drop('Experiment',axis=1)
+
+	SAMEA = X.iloc[362:466] 
+	SAMEA_targets = SAMEA['Experiment'] #Target prediction set
+	SAMEA = SAMEA.drop('Experiment', axis=1) #Feature prediction set
+
+	#Scale the data
+	CCMD = StandardScaler().fit_transform(CCMD)
+	ERR = StandardScaler().fit_transform(ERR) 
+	MMRS = StandardScaler().fit_transform(MMRS) 
+	SAMEA = StandardScaler().fit_transform(SAMEA) 
+
+	#Call return logistic regression function calls
+	return logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_2, losoTargetTrain=y_temp_2, losoFeaturePredict=CCMD, losoTargetPredict=CCMD_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_3, losoTargetTrain=y_temp_3, losoFeaturePredict=ERR, losoTargetPredict=ERR_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_4, losoTargetTrain=y_temp_4, losoFeaturePredict=MMRS, losoTargetPredict=MMRS_targets), logisticRegeression(X,Y, loso=True,losoFeatureTrain=X_temp_5, losoTargetTrain=y_temp_5, losoFeaturePredict=SAMEA, losoTargetPredict=SAMEA_targets)
+
 
 
 #Call methods and do final processing
-
+#X = pca(X)
 #binaryData(X,0.001) #Make features binary (1 if above threshold 0 if below)
 
-LOSONoUS(X,Y)
 #LOSO(X,Y)
+#LOSONoUS(X,Y)
+#LOSONoFrance(X,Y)
 #kneighbors(X, Y)
-#logisticRegeression(X,Y)
+logisticRegeression(X,Y)
 #lassoRegression(X, Y)
 
